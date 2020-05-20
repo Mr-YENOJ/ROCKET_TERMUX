c04926df7d2053eb9e0f062d432f9c17ce1613c6
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-05-20 20:13:40 +0530

    Create start_rocket.sh

---
+++ start_rocket.sh
@@ -0,0 +1,30 @@
+#Thanku for 1k subs 
+#This is a special tool for my subs
+#plzzz don't try to copy this script plzz 
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+clear
+echo "$red
+
+████████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗        ██████╗ 
+╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝        ██╔══██╗
+   ██║   █████╗  ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝         ██████╔╝
+   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗         ██╔══██╗
+   ██║   ███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗███████╗██║  ██║
+   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ "$rset  
+echo "$blue      >>>>>>>>>>[Press Enter To Continue]<<<<<<<<<<<<< "$rset
+read 1
+echo ""
+sleep 3.0
+echo ""
+bash pkg.txt
+echo ""
+clear
+echo ""
+termux-setup-storage
+echo ""
