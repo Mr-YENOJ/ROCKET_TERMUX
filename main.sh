3126d130439995ebb24ecc650795274ec081544b
Inderveerjoney <62381493+Inderveerjoney@users.noreply.github.com>
2020-05-20 21:08:37 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,6 @@ echo ""
 echo -e "write your cowsay name here.."
 read kkcowsay
 echo ""
-echo "cowsay -f dragon "$kkcowsay" | lolcat ; php /data/data/com.termux/files/usr/etc/hi.php | lolcat" > cs.txt
+echo "cowsay -f yenoj "$kkcowsay" | lolcat ; php /data/data/com.termux/files/usr/etc/hi.php | lolcat" > cs.txt
 cat "cs.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo ""
